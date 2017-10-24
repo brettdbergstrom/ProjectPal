1947094c3eed53ee7bd28ddf1315da7d36450e09
Brett <bergie003@gmail.com>
2017-10-24 16:33:47 -0700

    initial project files

---
+++ WELCOME/js/script.js
@@ -0,0 +1,11 @@
+// get scripting
+$(document).ready(function() {
+    $('#createaccount').mouseenter(function() {
+        $(this).fadeTo('fast', 1);
+    });
+    
+    $('#createaccount').mouseleave(function() {
+        $(this).fadeTo('fast',0.5);
+    });
+    
+});
